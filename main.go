3f7f0c1b1377ee6ae032a5fecb71b2bbdfe4a41f
Ric Allinson <capecodehq@yahoo.co.uk>
2014-09-27 16:28:03 -0700

    Added support for vplot commands

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"image/color"
 	"image/png"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type plot struct {
@@ -16,23 +18,83 @@ type plot struct {
 	y   int
 }
 
-func read(f *os.File) (plots []plot) {
-
+func readPlotFile(f *os.File) (plots []*plot) {
 	scanner := bufio.NewScanner(f)
-
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := strings.Split(scanner.Text(), " ")
+		cmd := line[0]
+		x, _ := strconv.Atoi(line[1])
+		y, _ := strconv.Atoi(line[2])
+		plots = append(plots, &plot{cmd, x, y})
 	}
-
 	return plots
 }
 
-func draw(plots []plot) *image.Gray {
-	// Work out the size from the source vplot file.
-	img := image.NewGray(image.Rect(0, 0, 100, 100))
-	for x := 20; x < 80; x++ {
-		y := x/3 + 15
-		img.Set(x, y, color.Black)
+func getCanvasSize(plots []*plot) (x, y int) {
+	for _, p := range plots {
+		if x < p.x {
+			x = p.x
+		}
+		if y < p.y {
+			y = p.y
+		}
+	}
+	return x, y
+}
+
+func abs(x int) int {
+	if x >= 0 {
+		return x
+	}
+	return -x
+}
+
+func drawline(x0, y0, x1, y1 int, img *image.RGBA) {
+	dx := abs(x1 - x0)
+	dy := abs(y1 - y0)
+	sx, sy := 1, 1
+	if x0 >= x1 {
+		sx = -1
+	}
+	if y0 >= y1 {
+		sy = -1
+	}
+	err := dx - dy
+	for {
+		img.Set(x0, y0, color.Black)
+		if x0 == x1 && y0 == y1 {
+			return
+		}
+		e2 := err * 2
+		if e2 > -dy {
+			err -= dy
+			x0 += sx
+		}
+		if e2 < dx {
+			err += dx
+			y0 += sy
+		}
+	}
+}
+
+func draw(plots []*plot) *image.RGBA {
+	w, h := getCanvasSize(plots)
+	img := image.NewRGBA(image.Rect(0, 0, w+1, h+1))
+	cx, cy := 0, 0
+	// fmt.Printf("x: %v, y: %v\n", cx, cy)
+	for _, p := range plots {
+        if p.cmd == "m" {
+            drawline(cx, cy, cx+p.x, cy+p.y, img)
+            cx, cy = cx+p.x, cy+p.y
+        } else if p.cmd == "M" {
+            drawline(cx, cy, p.x, p.y, img)
+            cx, cy = p.x, p.y
+        } else if p.cmd == "L" {
+            cx, cy = p.x, p.y
+        } else if p.cmd == "l" {
+            cx, cy = p.x, p.y
+        }
+		// fmt.Printf("x: %v, y: %v\n", cx, cy)
 	}
 	return img
 }
@@ -60,7 +122,7 @@ func main() {
 		return
 	}
 
-	destData := draw(read(vplotData))
+	destData := draw(readPlotFile(vplotData))
 
 	file, _ := os.Create(dest)
 	defer file.Close()
