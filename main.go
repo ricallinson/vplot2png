064d7e48eec9b46efe49f77c8814cd2d5446b78f
Ric Allinson <capecodehq@yahoo.co.uk>
2014-12-26 12:53:00 -0800

    Added pen size option

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func abs(x int) int {
 	return -x
 }
 
-func drawline(x0, y0, x1, y1 int, img *image.RGBA) {
+func drawline(x0, y0, x1, y1 int, img *image.RGBA, penSize int) {
 	dx := abs(x1 - x0)
 	dy := abs(y1 - y0)
 	sx, sy := 1, 1
@@ -61,7 +61,9 @@ func drawline(x0, y0, x1, y1 int, img *image.RGBA) {
 	}
 	err := dx - dy
 	for {
-		img.Set(x0, y0, color.Black)
+		for s := 0; s <= penSize; s++ {
+			img.Set(x0 + s, y0 + s, color.Black)
+		}
 		if x0 == x1 && y0 == y1 {
 			return
 		}
@@ -77,17 +79,17 @@ func drawline(x0, y0, x1, y1 int, img *image.RGBA) {
 	}
 }
 
-func draw(plots []*plot) *image.RGBA {
+func draw(plots []*plot, penSize int) *image.RGBA {
 	w, h := getCanvasSize(plots)
 	img := image.NewRGBA(image.Rect(0, 0, w+1, h+1))
 	cx, cy := 0, 0
 	// fmt.Printf("x: %v, y: %v\n", cx, cy)
 	for _, p := range plots {
 		if p.cmd == "l" {
-			drawline(cx, cy, cx+p.x, cy+p.y, img)
+			drawline(cx, cy, cx+p.x, cy+p.y, img, penSize)
 			cx, cy = cx+p.x, cy+p.y
 		} else if p.cmd == "L" {
-			drawline(cx, cy, p.x, p.y, img)
+			drawline(cx, cy, p.x, p.y, img, penSize)
 			cx, cy = p.x, p.y
 		} else if p.cmd == "m" {
 			cx, cy = cx+p.x, cy+p.y
@@ -100,6 +102,8 @@ func draw(plots []*plot) *image.RGBA {
 }
 
 func main() {
+
+	var penSize = flag.Int("p", 5, "the size of the pen tip used for the drawing")
 
 	flag.Parse()
 	vplot := flag.Arg(0)
@@ -122,7 +126,7 @@ func main() {
 		return
 	}
 
-	destData := draw(readPlotFile(vplotData))
+	destData := draw(readPlotFile(vplotData), *penSize)
 
 	file, _ := os.Create(dest)
 	defer file.Close()
